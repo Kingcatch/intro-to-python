c72feb15c05840eb4ea0f447aa61358eeb1d326e
Nana <nanagalaxy385@gmail.com>
2025-03-18 14:26:36 +0000

    simple addition function task complete

--- 015_add_numbers.py
+++ 015_add_numbers.py
@@ -11,7 +11,8 @@ from lib.helpers import check_that_these_are_equal
 # * Returns the result
 
 # YOUR FUNCTION GOES BELOW THIS LINE
-
+def add_numbers(num_1, num_2):
+    return num_1 + num_2
 
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
